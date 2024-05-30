7565f2dafb5e543fc3fd834509f872c7b1176307
patilkunal8 <patilkunal8@gmail.com>
2024-05-30 18:43:27 -0500

    update exception file

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+#sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from src.logger import logging
 def error_message_detail(error,error_detail:sys):
